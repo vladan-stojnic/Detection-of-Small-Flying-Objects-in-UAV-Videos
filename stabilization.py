9a4784e476759721f271d9baa06e08e490e0e82d
vladan-stojnic <35199816+vladan-stojnic@users.noreply.github.com>
2021-10-31 15:34:40 +0100

    LICENSE header

--- stabilization.py
+++ stabilization.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2021 Project Bee4Exp.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 """ Stablilizes a video using ORB detector.
 
 CL Args:
