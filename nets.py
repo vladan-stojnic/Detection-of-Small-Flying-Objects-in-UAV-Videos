686a50637a8bb965541be82438aa7e84a37ab265
vladan-stojnic <35199816+vladan-stojnic@users.noreply.github.com>
2021-10-31 15:33:55 +0100

    LICENSE header

--- nets.py
+++ nets.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2021 Project Bee4Exp.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 from keras.layers import Activation, Conv2D, MaxPooling2D, Input, BatchNormalization, UpSampling2D, Add
 from keras.models import Model
 
